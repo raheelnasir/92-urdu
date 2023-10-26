b591ab19f5da19687f2b3cbcdcbb99c01fad30fb
iraheeel <iraheeel@gmail.com>
2023-10-26 21:17:10 +0500

    New Layout

--- View/Authentication/CustomAuthenticationStateProvider.cs
+++ View/Authentication/CustomAuthenticationStateProvider.cs
@@ -21,7 +21,6 @@ namespace View.Authentication
         {
             try
             {
-                await Task.Delay(1000);
                 var userSessionStorageResult = await _sessionStorage.GetAsync<UserSession>("UserSession");
                 var userSession = userSessionStorageResult.Success ? userSessionStorageResult.Value : null;
                 if (userSession == null)
